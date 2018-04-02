1323b9fc5e622b6f37c3cbc1f2a340b00260652b
frblazqu <frblazqu@ucm.es>
2018-04-02 23:16:21 +0200

    Commit inicial de creación del proyecto

---
+++ src/main/java/ucm/fdi/tp/App.java
@@ -0,0 +1,13 @@
+package ucm.fdi.tp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
